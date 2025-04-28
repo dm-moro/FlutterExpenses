7729cd15c281bdb80393a2363062beed79113da8
Danielly Moro <daniellymoro12@email.com>
2025-04-28 10:09:47 -0300

    feat: Transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -12,13 +13,52 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  final _transactions = [
+    Transaction(id: 't1', title: 'Novo tênis de corrida', value: 310.79, date: DateTime.now()),
+    Transaction(id: 't2', title: 'Conta de luz', value: 110.09, date: DateTime.now()),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Despesas Pessoais")),
-      body: Center(child: Text('Versão inicial!!')),
+      appBar: AppBar(title: Text("Despesas Pessoais"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            child: Card(
+              elevation: 5,
+              color: Colors.blue[300],
+              child: Text(
+              'Gráfico',
+              textAlign: TextAlign.center),
+            ),
+          ),
+          Column(
+            children: _transactions.map((tr) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      child: Text(
+                        tr.value.toString(),
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text(tr.title),
+                        Text(tr.date.toString()),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          )
+        ],
+      ),
     );
   }
 }
