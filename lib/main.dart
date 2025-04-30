2c7528dda8c7e5fa2dc02d2ce45d925453d05fd0
Danielly Moro <daniellymoro12@email.com>
2025-04-30 10:18:54 -0300

    refactor: Layout update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import '../components/transaction_user.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -14,112 +14,28 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final _transactions = [
-    Transaction(id: 't1', title: 'Novo tênis de corrida', value: 310.79, date: DateTime.now()),
-    Transaction(id: 't2', title: 'Conta de luz', value: 110.09, date: DateTime.now()),
-  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Despesas Pessoais"),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            child: Card(
-              elevation: 5,
-              color: Colors.blue[300],
-              child: Text(
-              'Gráfico',
-              textAlign: TextAlign.center),
-            ),
-          ),
-          Column(
-            children: _transactions.map((tr) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin:  EdgeInsets.symmetric(
-                        horizontal: 15,
-                        vertical: 10
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2
-                        ),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        ' R\$ ${tr.value.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tr.title,
-                          style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold
-                          ),
-                        ),
-                        Text(
-                          DateFormat('d MMM y').format(tr.date),
-                          style: TextStyle(
-                          color: Colors.grey[600]
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
-          Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Título'
-                    ),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Valor R\$'
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      TextButton(
-                        onPressed: () {}, 
-                        child: Text(
-                          'Nova transação',
-                          style: TextStyle(
-                            color: Colors.purple
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Card(
+                elevation: 5,
+                color: Colors.blue[300],
+                child: Text(
+                'Gráfico',
+                textAlign: TextAlign.center),
               ),
             ),
-          )
-        ],
+            TransactionUser(),
+          ],
+        ),
       ),
     );
   }
